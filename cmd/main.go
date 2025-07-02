0920c93233c67d5a793d2c5b2b529bf16bc5738a
Vansh <vanshrepalto@gmail.com>
2025-07-02 17:19:44 +0530

    added start engine function in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/1sh-repalto/url-monitoring-api/internal/engine"
 	"github.com/1sh-repalto/url-monitoring-api/internal/handler"
 	"github.com/1sh-repalto/url-monitoring-api/internal/repository"
 	"github.com/1sh-repalto/url-monitoring-api/internal/router"
@@ -34,6 +35,10 @@ func main() {
 	// Initialize handlers
 	authHandler := handler.NewAuthHandler(authService)
 	urlHandler := handler.NewURLHandler(urlService)
+
+	// start monitoring engine
+	monitorEngine := engine.NewMonitorEngine(urlService)
+	go monitorEngine.Start()
 
 	// Setup router
 	r := router.SetupRoutes(authHandler, urlHandler)
